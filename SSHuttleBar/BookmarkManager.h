3bd8e4ed6eb49a24bcfe102da193a7edd3fd8204
Andrea Alberti <a.alberti82@gmail.com>
2024-02-11 12:54:50 +0100

    Initial commit

---
+++ SSHuttleBar/BookmarkManager.h
@@ -0,0 +1,16 @@
+//
+//  BookmarkManager.h
+//  SSHuttleBar
+//
+//  Created by Andrea Alberti on 11.02.24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BookmarkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SSHuttleBar/BookmarkManager.m
@@ -0,0 +1,12 @@
+//
+//  BookmarkManager.m
+//  SSHuttleBar
+//
+//  Created by Andrea Alberti on 11.02.24.
+//
+
+#import "BookmarkManager.h"
+
+@implementation BookmarkManager
+
+@end
