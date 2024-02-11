ce25f8de8f075f3664bcdc4564b856d762b65cdf
Andrea Alberti <a.alberti82@gmail.com>
2024-02-11 09:19:30 +0100

    Initial Commit

---
+++ SSHuttleBar/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SSHuttleBar
+//
+//  Created by Andrea Alberti on 11.02.24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
