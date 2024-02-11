0f4d6facc902dbf8ff1510b7b1f975fb2d8edeaa
Andrea Alberti <a.alberti82@gmail.com>
2024-02-11 22:39:48 +0100

    Completed first working version.

--- SSHuttleBar/AppDelegate.m
+++ SSHuttleBar/AppDelegate.m
@@ -7,20 +7,25 @@
 
 //#import "BookmarkManager.h"
 #import "AppDelegate.h"
-#import "CustomStatusBarButton.h"
+#import "CustomStatusBarView.h"
 
 @interface AppDelegate ()
 @property (strong, nonatomic) NSStatusItem *statusItem;
 @property (strong, nonatomic) NSTask *sshTask;
-@property (assign, nonatomic) BOOL isConnected;
+@property (strong, nonatomic) NSTask *sshuttleTask;
+@property (assign, nonatomic) BOOL isSSHconnected;
+@property (assign, nonatomic) BOOL isSSHuttleConnected;
 @property (strong, nonatomic) NSString *connectedIcon;
 @property (strong, nonatomic) NSString *disconnectedIcon;
 @property (strong, nonatomic) NSURL *bookmarkURL; // Property to store the bookmark URL
-@property (assign, nonatomic) BOOL shouldMonitorSSH;
-@property (assign, nonatomic) BOOL isThreadSleeping;
+@property (assign, nonatomic) BOOL autorestartSSHuttle;
+@property (assign, nonatomic) BOOL autorestartSSH;
 @property (strong, nonatomic) NSLock *lock;
 @property (strong, nonatomic) dispatch_semaphore_t finishedThreadCycled;
 @property (assign, nonatomic) BOOL connectAfterLaunch;
+@property (assign, nonatomic) NSNumber *connType;
+@property (assign, nonatomic) NSMenuItem* item_ssh;
+@property (assign, nonatomic) NSMenuItem* item_direct;
 @end
 
 @implementation AppDelegate
@@ -38,7 +43,7 @@
         NSError *error = nil;
         [fileManager createDirectoryAtPath:appSpecificPrefsDirectory withIntermediateDirectories:YES attributes:nil error:&error];
         if (error) {
-            NSLog(@"Error creating app-specific preferences directory: %@", error.localizedDescription);
+            NSLog(@"SSHuttleBar: Error creating app-specific preferences directory: %@", error.localizedDescription);
             return nil; // Handle the error appropriately
         }
     }
@@ -48,19 +53,22 @@
     return prefsFilePath;
 }
 
-- (void)applicationWillTerminate:(NSNotification *)notification {
+- (void)terminate_processes {
+    [self terminateSSHuttleProcess];
     [self terminateSSHprocess];
-    
-    if ([self.sshTask isRunning]) {
-        [self.sshTask terminate];
-    }
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notification {
+    NSLog(@"SSHuttleBar: Exiting SSHuttleBar");
+    [self terminate_processes];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     
     NSString *prefsFilePath = [self getCustomPrefsFilePath];
     NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:prefsFilePath];
-    [self setConnectAfterLaunch:[([prefs objectForKey:@"connectAfterLaunch"] ? : 0) boolValue]];
+    [self setConnectAfterLaunch:[([prefs objectForKey:@"connectAfterLaunch"] ? : @0) boolValue]];
+    [self setConnType:[prefs objectForKey:@"connectType"] ? : @0];
     
     self.lock = [[NSLock alloc] init];
     
@@ -69,6 +77,7 @@
     
     // Initialize your status item here as before
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
     //self.statusItem.button.action = @selector(statusItemClicked:);
     self.statusItem.button.target = self;
     
@@ -78,58 +87,41 @@
     // Setup the right-click menu
     [self setupStatusItemMenu];
     
-    self.isThreadSleeping = NO; // Initially, the thread is not sleeping
-    self.shouldMonitorSSH = NO;
     if([self connectAfterLaunch]) {
-        self.shouldMonitorSSH = YES;
-        [self monitorSSHProcess];
+        [self start_processes];
     }
 }
 
-- (void)monitorSSHProcess {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        BOOL wasDisconnected = false;
-        while (self.shouldMonitorSSH) {
-            [self.lock lock];
-            self.finishedThreadCycled = dispatch_semaphore_create(0);
-            [self.lock unlock];
-            
-            if (!self.sshTask.isRunning) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    self.statusItem.button.title = self.disconnectedIcon;
-                    // NSLog(@"Change status");
-                });
-                wasDisconnected = true;
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [self startSSHProcess]; // Attempt to start the process again
-                    NSLog(@"Restarted");
-                });
-            } else {
-                if(wasDisconnected){
-                    wasDisconnected = false;
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        self.statusItem.button.title = self.connectedIcon;
-                    });
-                }
-            }
-            
-            dispatch_semaphore_signal(self.finishedThreadCycled); // Signal that thread is finished
-            
-            [self.lock lock];
-            self.isThreadSleeping = YES; // Thread is about to sleep
-            [self.lock unlock];
-            sleep(1); // Check every second
-            [self.lock lock];
-            self.isThreadSleeping = NO; // Thread is about to sleep
-            [self.lock unlock];
-        }
-    });
+- (void)start_processes{
+    if([[self connType] intValue]==0){
+        [self startSSHProcess];
+        NSLog(@"SSHuttleBar: Connected to the ssh process");
+    }
+    [self startSSHuttleProcess];
+    NSLog(@"SSHuttleBar: Connected to the sshuttle process");
 }
 
 - (void)setupStatusItemMenu {
     NSMenu *menu = [[NSMenu alloc] init];
-    NSMenuItem* item = [menu addItemWithTitle:@"Connect after launch" action:@selector(toggleAutoConnect:) keyEquivalent:@"a"];
-    [item setState:[self connectAfterLaunch]];
+    
+    [self setItem_ssh:[menu addItemWithTitle:@"Connect through SSH" action:@selector(selectSSHconnect:) keyEquivalent:@"0"]];
+    [self setItem_direct:[menu addItemWithTitle:@"Connect directly" action:@selector(selectDirectConnect:) keyEquivalent:@"1"]];
+    
+    switch ([[self connType] intValue]) {
+        case 0:
+            [[self item_ssh] setState:YES];
+            break;
+        case 1:
+        default:
+            [[self item_direct] setState:YES];
+            break;
+    }
+    
+    [menu addItem:[NSMenuItem separatorItem]];
+    
+    NSMenuItem* item_activate = [menu addItemWithTitle:@"Activate on launch" action:@selector(toggleAutoConnect:) keyEquivalent:@"a"];
+    [item_activate setState:[self connectAfterLaunch]];
+    
     [menu addItemWithTitle:@"Quit" action:@selector(quitApp:) keyEquivalent:@"q"];
     
     [self.statusItem setMenu:menu]; // This enables right-click menu
@@ -139,81 +131,225 @@
     [NSApp terminate:nil];
 }
 
+- (void) selectSSHconnect:(NSMenuItem*)menuItem {
+    
+    BOOL current_status_ssh = [[self item_ssh] state];
+    BOOL current_status_direct = [[self item_direct] state];
+    
+    if( current_status_ssh || current_status_direct)
+    {
+        [self terminate_processes];
+    }
+    
+    [self setConnType:@0];
+    
+    if(!current_status_ssh){
+        [self start_processes];
+    }
+    
+    [[self item_ssh] setState:!current_status_ssh];
+    [[self item_direct] setState:NO];
+    
+    NSString *prefsFilePath = [self getCustomPrefsFilePath];
+    NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:prefsFilePath] ?: [NSMutableDictionary dictionary];
+    
+    // Update the connectAfterLaunch field in the dictionary
+    prefs[@"connectType"] = [self connType];
+    
+    // Write the updated dictionary back to the preferences file
+    BOOL success = [prefs writeToFile:prefsFilePath atomically:YES];
+    if (!success) {
+        NSLog(@"SSHuttleBar: Failed to save preferences");
+    }
+}
+
+- (void) selectDirectConnect:(NSMenuItem*)menuItem {
+    BOOL current_status_ssh = [[self item_ssh] state];
+    BOOL current_status_direct = [[self item_direct] state];
+    
+    if( current_status_ssh || current_status_direct)
+    {
+        [self terminate_processes];
+    }
+    
+    [self setConnType:@1];
+    
+    if(!current_status_direct){
+        [self start_processes];
+    }
+    
+    [[self item_ssh] setState:NO];
+    [[self item_direct] setState:!current_status_direct];
+        
+    NSString *prefsFilePath = [self getCustomPrefsFilePath];
+    NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:prefsFilePath] ?: [NSMutableDictionary dictionary];
+    
+    // Update the connectAfterLaunch field in the dictionary
+    prefs[@"connectType"] = [self connType];
+    
+    // Write the updated dictionary back to the preferences file
+    BOOL success = [prefs writeToFile:prefsFilePath atomically:YES];
+    if (!success) {
+        NSLog(@"SSHuttleBar: Failed to save preferences");
+    }
+}
+
 - (void)toggleAutoConnect:(NSMenuItem*)menuItem {
     [self setConnectAfterLaunch:![self connectAfterLaunch]];
     
     // Update the menu item state to reflect the toggle
     menuItem.state=self.connectAfterLaunch ? NSControlStateValueOn : NSControlStateValueOff;
-
+    
     NSString *prefsFilePath = [self getCustomPrefsFilePath];
     NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:prefsFilePath] ?: [NSMutableDictionary dictionary];
     
     // Update the connectAfterLaunch field in the dictionary
     prefs[@"connectAfterLaunch"] = [NSNumber numberWithBool:[self connectAfterLaunch]];
-
+    
     // Write the updated dictionary back to the preferences file
     BOOL success = [prefs writeToFile:prefsFilePath atomically:YES];
     if (!success) {
-        NSLog(@"Failed to save preferences");
+        NSLog(@"SSHuttleBar: Failed to save preferences");
     }
 }
 
 - (void)startSSHProcess {
+    self.autorestartSSH = YES;
     self.sshTask = [[NSTask alloc] init];
     [self.sshTask setLaunchPath:@"/usr/bin/ssh"];
     [self.sshTask setArguments:@[@"-N", @"-L", @"8022:localhost:8022", @"-i", @"/Users/andrea/.ssh/id_computing-server_bonn", @"computing-server2.iap.uni-bonn.de"]];
     
+    // Redirect output and error to /dev/null
+    NSFileHandle *nullFileHandle = [NSFileHandle fileHandleWithNullDevice];
+    [self.sshTask setStandardOutput:nullFileHandle];
+    [self.sshTask setStandardError:nullFileHandle];
+
     // Handle ssh task termination
     __weak typeof(self) weakSelf = self;
     [self.sshTask setTerminationHandler:^(NSTask *task) {
         dispatch_async(dispatch_get_main_queue(), ^{
-            __strong typeof(weakSelf) strongSelf = weakSelf;
-            strongSelf.isConnected = NO;
-            [strongSelf updateConnectionStatus:NO];
+            NSLog(@"SSHuttleBar: Lost ssh connection");
+            __strong typeof(weakSelf) strongSelf1 = weakSelf;
+            strongSelf1.isSSHconnected = NO;
+            [strongSelf1 updateConnectionStatus];
+            
+            if ([strongSelf1 autorestartSSH]) {
+                // Wait for 1 second before trying to restart the SSH process
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    __strong typeof(weakSelf) strongSelf2 = weakSelf;
+                    [strongSelf2 setAutorestartSSHuttle:NO];
+                    [strongSelf2 terminateSSHuttleProcess];
+                    
+                    [strongSelf2 startSSHProcess];
+                    NSLog(@"SSHuttleBar: Reconnected to the ssh process");
+                    
+                    [strongSelf2 setAutorestartSSHuttle:YES];
+                    [strongSelf2 startSSHuttleProcess];
+                    NSLog(@"SSHuttleBar: Reconnected to the sshuttle process");
+                });
+            };
+        });
+    }];
+
+    // Start the ssh process
+    [self.sshTask launch];
+    self.statusItem.button.title = self.connectedIcon;
+}
+
+- (void)startSSHuttleProcess {
+    self.autorestartSSHuttle = YES;
+    self.sshuttleTask = [[NSTask alloc] init];
+    [self.sshuttleTask setLaunchPath:@"/usr/local/bin/sshuttle"];
+    switch ([[self connType] intValue]) {
+        case 0:
+            [self.sshuttleTask setArguments:@[@"-r", @"m1-gateway", @"--dns", @"0/0"]];
+            break;
+        case 1:
+        default:
+            [self.sshuttleTask setArguments:@[@"-r", @"m1-gateway-local", @"--dns", @"0/0"]];
+            break;
+    }
+    
+    // Redirect output and error to /dev/null
+    NSFileHandle *nullFileHandle = [NSFileHandle fileHandleWithNullDevice];
+    [self.sshuttleTask setStandardOutput:nullFileHandle];
+    [self.sshuttleTask setStandardError:nullFileHandle];
+    
+    // Handle ssh task termination
+    __weak typeof(self) weakSelf = self;
+    [self.sshuttleTask setTerminationHandler:^(NSTask *task) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"SSHuttleBar: Lost sshuttle connection");
+            __strong typeof(weakSelf) strongSelf1 = weakSelf;
+            strongSelf1.isSSHuttleConnected = NO;
+            [strongSelf1 updateConnectionStatus];
+            
+            BOOL relaunch = false;
+            if([[self connType] intValue] == 1){
+                relaunch = true;
+            }
+            [strongSelf1.lock lock];
+            if ([strongSelf1.sshTask isRunning]) {
+                relaunch = true;
+            }
+            [strongSelf1.lock unlock];
+            if (relaunch && [strongSelf1 autorestartSSHuttle]) {
+                // Wait for 1 second before trying to restart the SSH process
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    __strong typeof(weakSelf) strongSelf2 = weakSelf;
+                    [strongSelf2 startSSHuttleProcess];
+                    NSLog(@"SSHuttleBar: Reconnected to the sshuttle process");
+                });
+            }
+            
         });
     }];
     
     // Start the ssh process
-    [self.sshTask launch];
-    
-    
-    // Optionally, start monitoring the process in a separate thread or via dispatch queue here
+    [self.sshuttleTask launch];
+    self.statusItem.button.title = self.connectedIcon;
 }
 
 - (void)terminateSSHprocess {
-    NSLog(@"Terminate the ssh process");
-    self.shouldMonitorSSH = NO;
-    if (!self.isThreadSleeping) {
-        // Wait for the thread to enter the sleep phase
-        dispatch_semaphore_wait(self.finishedThreadCycled, DISPATCH_TIME_FOREVER);
+    self.autorestartSSH = NO;
+    if ([self.sshTask isRunning]) {
+        NSLog(@"SSHuttleBar: Terminate the ssh process");
+        [self.sshTask terminate];
     }
-    [self.sshTask terminate];
+}
+
+- (void)terminateSSHuttleProcess {
+    self.autorestartSSHuttle = NO;
+    if ([self.sshuttleTask isRunning]) {
+        NSLog(@"SSHuttleBar: Terminate the sshuttle process");
+        [self.sshuttleTask terminate];
+    }
 }
 
 - (void)statusItemClicked:(id)sender {
-    if (self.isConnected) {
+    if (self.isSSHconnected) {
         // Terminate the ssh process
         [self terminateSSHprocess];
     } else {
         // Start the ssh process
-        NSLog(@"Start the ssh process");
-        self.shouldMonitorSSH = YES;
-        [self monitorSSHProcess];
+        NSLog(@"SSHuttleBar: Start the ssh process");
     }
     // Toggle the connection status
-    self.isConnected = !self.isConnected;
+    self.isSSHconnected = !self.isSSHconnected;
     // Update the icon based on the new connection status
-    [self updateConnectionStatus:self.isConnected];
+    [self updateConnectionStatus];
 }
 
-- (void)updateConnectionStatus:(BOOL)connected {
+- (void)updateConnectionStatus {
+    
+    BOOL connected = [self isSSHuttleConnected] && ([self isSSHconnected] || ([[self connType] intValue] == 1));
+    
     if (connected) {
         self.statusItem.button.title = self.connectedIcon; // Connected icon
     } else {
-        self.statusItem.button.title = self.disconnectedIcon; // Disconnected icon
+        self.statusItem.button.title = self.disconnectedIcon; // DisSSHconnected icon
     }
 }
-
 
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
     return YES;
